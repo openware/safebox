f2b5756b5c8f96094389bf78b937a0b7855366e4
Kohelbekker <kohelbekker@gmail.com>
2019-12-05 20:58:16 +0200

    masterkey request and response for vault checked

--- pkg/tools/vault.go
+++ pkg/tools/vault.go
@@ -1,9 +1,13 @@
-package tools
+package main
 
 import (
+	"encoding/hex"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil/hdkeychain"
 	"github.com/hashicorp/vault/api"
 )
 
@@ -29,22 +33,52 @@ func InitVault(token string) error {
 	return nil
 }
 
+func storeMasterKey(masterKey *hdkeychain.ExtendedKey) error {
+	pathArg := "cubbyhole/masterkey"
+	c := VaultClient.Logical()
+	_, err := c.Write(pathArg,
+		map[string]interface{}{
+			"key": masterKey.String(),
+		})
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+func getMasterKey() (interface{}, error) {
+	pathArg := "cubbyhole/masterkey"
+
+	c := VaultClient.Logical()
+	v, err := c.Read(pathArg)
+	if err != nil {
+		return nil, fmt.Errorf("%v", err)
+	}
+	if v == nil {
+		return nil, fmt.Errorf("value not found!")
+	}
+	if _, err := v.Data["key"]; !err {
+		return nil, fmt.Errorf("such key doesn't exist!")
+	}
+	return v.Data["key"], nil
+}
+
 func main() {
 	err := InitVault(vaultToken)
 	if err != nil {
 		log.Println(err)
+		return
 	}
-	c := VaultClient.Logical()
+	testVec1MasterHex := "000102030405060708090a0b0c0d0e0f"
+	masterSeed, err := hex.DecodeString(testVec1MasterHex)
+	masterKey, err := hdkeychain.NewMaster(masterSeed, &chaincfg.MainNetParams)
 
-	pathArg := "cubbyhole/mysecret"
-	secret, err := c.Write(pathArg,
-		map[string]interface{}{
-			"name":     "Louis",
-			"username": "mod",
-			"password": "pw",
-		})
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println(secret)
+	storeMasterKey(masterKey)
+	masterKey2, _ := getMasterKey()
+
+	masterKey2 = fmt.Sprintf("%v", masterKey2)
+
+	fmt.Println("Master key1: ", masterKey.String())
+	fmt.Println("Master key2: ", masterKey2)
 }
